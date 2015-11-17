bdbfae85bfdc48f44c40f2ebe76c1bf69c254660
Bernhard Johannessen <bernhard@voytsje.com>
2015-11-17 03:13:51 +0000

    Added cryptographic random number generator

---
+++ Algorithms.Tests/CryptoRandomTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+    [TestFixture]
+    public class CryptoRandomTests
+    {
+        [Test]
+        public void Next()
+        {
+            var random = new CryptoRandom();
+            int next = -1;
+            Assert.DoesNotThrow(() => next = random.Next());
+            Assert.That(next, Is.GreaterThanOrEqualTo(0));
+        }
+
+        [Test]
+        public void Next_maxvalue()
+        {
+            var random = new CryptoRandom();
+            int next = -1;
+            Assert.DoesNotThrow(() => next = random.Next(1000));
+            Assert.That(next, Is.GreaterThanOrEqualTo(0));
+            Assert.That(next, Is.LessThan(1000));
+
+            next = -1;
+            Assert.DoesNotThrow(() => next = random.Next(500));
+            Assert.That(next, Is.GreaterThanOrEqualTo(0));
+            Assert.That(next, Is.LessThan(500));
+        }
+
+        [Test]
+        public void Next_minvalue_maxvalue()
+        {
+            var random = new CryptoRandom();
+            int next = 0;
+            Assert.DoesNotThrow(() => next = random.Next(20, 100));
+            Assert.That(next, Is.GreaterThanOrEqualTo(20));
+            Assert.That(next, Is.LessThan(100));
+
+            next = 0;
+            Assert.DoesNotThrow(() => next = random.Next(-40, -20));
+            Assert.That(next, Is.GreaterThanOrEqualTo(-40));
+            Assert.That(next, Is.LessThan(-20));
+        }
+
+        [Test]
+        public void NextBytes()
+        {
+            var random = new CryptoRandom();
+            var buffer = new byte[10];
+            Assert.DoesNotThrow(() => random.NextBytes(buffer));
+        }
+
+        [Test]
+        public void NextDouble()
+        {
+            var random = new CryptoRandom();
+            double next = -1.0;
+            Assert.DoesNotThrow(() => next = random.NextDouble());
+            Assert.That(next, Is.GreaterThanOrEqualTo(0.0));
+            Assert.That(next, Is.LessThanOrEqualTo(1.0));
+
+            next = -1.0;
+            Assert.DoesNotThrow(() => next = random.NextDouble());
+            Assert.That(next, Is.GreaterThanOrEqualTo(0.0));
+            Assert.That(next, Is.LessThanOrEqualTo(1.0));
+        }
+
+        [Test]
+        public void Sample()
+        {
+            var random = new CryptoRandom();
+            double next = -1.0;
+            Assert.DoesNotThrow(() => next = random.NextDouble());
+            Assert.That(next, Is.GreaterThanOrEqualTo(0.0));
+            Assert.That(next, Is.LessThanOrEqualTo(1.0));
+
+            next = -1.0;
+            Assert.DoesNotThrow(() => next = random.NextDouble());
+            Assert.That(next, Is.GreaterThanOrEqualTo(0.0));
+            Assert.That(next, Is.LessThanOrEqualTo(1.0));
+        }
+    }
+}
---
+++ Algorithms/CryptoRandom.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Algorithms
+{
+    public class CryptoRandom : Random
+    {
+        private RandomNumberGenerator randomNumberGenerator;
+
+        public CryptoRandom()
+        {
+            randomNumberGenerator = RandomNumberGenerator.Create();
+        }
+
+        public override int Next()
+        {
+            return Next(0, int.MaxValue);
+        }
+
+        public override int Next(int maxValue)
+        {
+            return Next(0, maxValue);
+        }
+
+        public override int Next(int minValue, int maxValue)
+        {
+            return (int)Math.Round(NextDouble() * (maxValue - minValue - 1)) + minValue;
+        }
+
+        public override void NextBytes(byte[] buffer)
+        {
+            randomNumberGenerator.GetBytes(buffer);
+        }
+
+        public override double NextDouble()
+        {
+            var bytes = new byte[4];
+            randomNumberGenerator.GetBytes(bytes);
+            return (double)BitConverter.ToUInt32(bytes, 0) / UInt32.MaxValue;
+        }
+
+        protected override double Sample()
+        {
+            return NextDouble();
+        }
+    }
+}
