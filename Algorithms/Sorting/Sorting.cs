aab06517056418156371838a300aa3b9aa09ba1a
Bernhard Johannessen <bernhard@voytsje.com>
2015-11-17 03:24:56 +0000

    Move swap method to utility class

--- Algorithms/Sorting/Sorting.cs
+++ Algorithms/Sorting/Sorting.cs
@@ -8,7 +8,7 @@ namespace Algorithms.Sorting
     /// </summary>
     public static class Sorting
     {
-        private static Dictionary<SortingAlgorithm, Type> sortingAlgorithms;
+        static readonly Dictionary<SortingAlgorithm, Type> sortingAlgorithms;
 
         static Sorting()
         {
@@ -40,20 +40,6 @@ namespace Algorithms.Sorting
             var type = sortingAlgorithms[algorithm];
             var algorithmInstance = (ISortingAlgorithm)Activator.CreateInstance(type);
             return algorithmInstance.Sort(collection, key, sortOrder);
-        }
-
-        /// <summary>
-        /// Helper methods that swaps two values in a list
-        /// </summary>
-        /// <param name="list">The list with the values to swap</param>
-        /// <param name="indexA">The index of the first value to swap</param>
-        /// <param name="indexB">The index of the second value to swap</param>
-        /// <typeparam name="T">The type of the objects contained in the list</typeparam>
-        internal static void Swap<T>(this List<T> list, int indexA, int indexB)
-        {
-            T temp = list[indexA];
-            list[indexA] = list[indexB];
-            list[indexB] = temp;
         }
     }
 }
---
+++ Algorithms/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Helper methods that swaps two values in a list
+        /// </summary>
+        /// <param name="list">The list with the values to swap</param>
+        /// <param name="indexA">The index of the first value to swap</param>
+        /// <param name="indexB">The index of the second value to swap</param>
+        /// <typeparam name="T">The type of the objects contained in the list</typeparam>
+        internal static void Swap<T>(this IList<T> list, int indexA, int indexB)
+        {
+            T temp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = temp;
+        }
+    }
+}
